f123b419881b24a8a1e75802f4afb4db22ea4679
nursapa <71829100+nursapa@users.noreply.github.com>
2023-01-10 11:31:51 +0600

    Add files via upload

---
+++ kiryl's task/script.js
@@ -0,0 +1,36 @@
+const snowConteiner = document.getElementById("root");
+
+const random = (num) => {
+  return Math.floor(Math.random() * num);
+};
+
+const colors = ["red", "green", "blue", "yellow", "pink"];
+
+const getRandomStyles = () => {
+  const top = random(100);
+  const left = random(100);
+  const dur = random(5) + 3;
+  const size = random(25) + 25;
+  const width = random(5) + 5;
+  const background = random(5);
+
+  return `top: -${top}%;
+    left: ${left}%;
+    font-size: ${size}px;
+    animation-duration: ${dur}s;
+    width: ${width}px;
+    height: ${width}px;
+    background: ${colors[background]};
+    border-radius: 50%;
+    `;
+};
+
+const createSnows = (num) => {
+  for (let i = num; i > 0; i--) {
+    const snow = document.createElement("div");
+    snow.className = "snow";
+    snow.style.cssText = getRandomStyles();
+    snow.innerHTML;
+    snowConteiner.append(snow);
+  }
+};
